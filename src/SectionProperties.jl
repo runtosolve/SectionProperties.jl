2935130dc89d0ea5ef524959fa188f5fab090217
Cris Moen <cris.moen@runtosolve.com>
2022-04-26 09:56:41 -0400

    add new open cross-section generation functions

--- src/SectionProperties.jl
+++ src/SectionProperties.jl
@@ -1323,6 +1323,120 @@ end
 
 
 
+function lay_out_cross_section_nodes(L, θ)
+
+    num_segments = length(L)
+
+    cross_section = Array{Matrix{Float64}}(undef, num_segments)
+
+    for i in eachindex(L)
+
+        if i == 1
+
+            start_node = [0.0, 0.0]
+
+        else
+
+            start_node = vec(cross_section[i-1][end, :])
+
+        end
+
+        cross_section[i] = Geometry.transform_line_segment_to_global(L[i], start_node, θ[i])
+
+    end
+
+    cross_section = [[[0.0 0.0]]; cross_section] #add start node at unity
+
+    return cross_section
+
+end
+
+
+
+function generate_cross_section_rounded_corners(cross_section_nodes, r, n)
+
+    corners = Array{Matrix{Float64}}(undef, length(r))
+
+   for i in eachindex(r)
+
+        A = vec(cross_section_nodes[i]) #need vector format here to fillet calculations
+        B = vec(cross_section_nodes[i+1])
+        C = vec(cross_section_nodes[i+2])
+    
+        corners[i] = Geometry.generate_fillet(A, B, C, r[i], n[i])
+
+   end
+
+   return corners
+
+end
+
+
+
+
+
+function generate_straight_line_segments(cross_section_nodes, corners, n)
+
+    segments = Array{Matrix{Float64}}(undef, length(n))
+
+    corner_index = 1
+
+    for i in eachindex(n)
+
+        if i == 1
+
+            A = vec(cross_section_nodes[1])
+            B = corners[1][1,:]
+
+
+        elseif i == length(n)
+
+            A = corners[corner_index][end, :]
+            B = vec(cross_section_nodes[end])
+
+        else
+            A = corners[corner_index][end, :]
+            corner_index += 1
+            B = corners[corner_index][1, :]
+
+        end
+
+        segments[i] = Geometry.discretize_segment(A, B, n[i])
+
+    end
+
+    return segments
+
+end
+
+function generate_open_cross_section(L, θ, n, n_radius)
+
+    cross_section_nodes = lay_out_cross_section_nodes(L, θ)
+
+    corners = generate_cross_section_rounded_corners(cross_section_nodes, r, n_radius)
+
+    flats = generate_straight_line_segments(cross_section_nodes, corners, n)
+
+    num_flats = size(flats)[1]
+    num_corners = size(corners)[1]
+
+    num_segments = num_flats + num_corners
+
+    cross_section = [flats; corners]
+
+    cross_section_index = [1:2:num_segments; 2:2:num_segments]
+
+    cross_section_index_sorted = sortperm(cross_section_index)
+
+    cross_section = cross_section[cross_section_index_sorted]
+
+    cross_section = vcat(cross_section...)
+
+    return cross_section
+
+end
+
+
 end #module
 
 
